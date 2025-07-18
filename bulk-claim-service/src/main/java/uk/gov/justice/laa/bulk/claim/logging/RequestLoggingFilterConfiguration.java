cb69018075c2b013aa692d6ef0bd5b6ad5b136ac
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2025-07-18 12:48:59 +0100

    Fix checkstyle

--- bulk-claim-service/src/main/java/uk/gov/justice/laa/bulk/claim/logging/RequestLoggingFilterConfiguration.java
+++ bulk-claim-service/src/main/java/uk/gov/justice/laa/bulk/claim/logging/RequestLoggingFilterConfiguration.java
@@ -4,9 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
+/**
+ * Configuration class for request logging filter. This filter logs incoming requests, including
+ * query strings and payloads.
+ */
 @Configuration
 public class RequestLoggingFilterConfiguration {
 
+  /**
+   * Creates a {@link CommonsRequestLoggingFilter} bean. This filter logs request details such as
+   * query strings and payloads.
+   *
+   * @return the configured request logging filter
+   */
   @Bean
   public CommonsRequestLoggingFilter logFilter() {
     CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
