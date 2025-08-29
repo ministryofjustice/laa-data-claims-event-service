c9700142091543a4063c3a033f39ba7487a1e2f3
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-08-29 16:43:58 +0100

    CCMSPUI-805 | Fixes to tests

--- data-claims-event-service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsevent/model/BulkSubmissionMessage.java
+++ data-claims-event-service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsevent/model/BulkSubmissionMessage.java
@@ -4,6 +4,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Record for deserializing a bulk submission message from an SQS queue.
+ *
+ * @param bulkSubmissionId the bulk submission ID
+ * @param submissionIds the list of submission ID's
+ * @author Jamie Briggs
+ */
 public record BulkSubmissionMessage(
     @JsonProperty("bulk_submission_id") UUID bulkSubmissionId,
     @JsonProperty("submission_ids") List<UUID> submissionIds) {}
