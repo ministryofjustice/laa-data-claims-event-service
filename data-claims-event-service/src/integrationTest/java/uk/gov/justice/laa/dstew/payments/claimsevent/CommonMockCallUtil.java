831e55e771781657983b736d81ddd8170ceb5260
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-10-02 11:42:06 +0100

    Added ActiveProfile annotation to SubmissionValidationServiceIntegrationTest, removed unused test helper class, and uncommented out claims API wiremock in docker-compose.yml
    Signed-off-by: Jamie Briggs <jamie.briggs@justice.gov.uk>

--- data-claims-event-service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsevent/CommonMockCallUtil.java
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.laa.dstew.payments.claimsevent;
-
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
-public final class CommonMockCallUtil {}
