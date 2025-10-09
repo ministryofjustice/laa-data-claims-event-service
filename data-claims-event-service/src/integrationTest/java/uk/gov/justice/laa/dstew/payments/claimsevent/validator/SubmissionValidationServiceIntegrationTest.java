38db185567a88c3ffb8834c126be6b8067a386c3
Dibyatanu Deb <dibyatanu.deb1@justice.gov.uk>
2025-10-09 14:14:44 +0100

    Bc 71 duplicate checks submission period data claims event service (#36)
    * BC-71 added get submission to data claims rest client
    
    * BC-71 added get submission to data claims rest client
    
    * BC-71 Duplicate Checks - Submission Period
    
    * BC-71 addressed PR comments

--- data-claims-event-service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsevent/validator/SubmissionValidationServiceIntegrationTest.java
+++ data-claims-event-service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsevent/validator/SubmissionValidationServiceIntegrationTest.java
@@ -4,10 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.justice.laa.dstew.payments.claimsevent.ContextUtil.assertContextClaimError;
 import static uk.gov.justice.laa.dstew.payments.claimsevent.ContextUtil.assertContextHasNoErrors;
 
+import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockserver.model.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -26,6 +28,9 @@ import uk.gov.justice.laa.dstew.payments.claimsevent.validation.SubmissionValida
 @DisplayName("Submission validation service integration tests")
 public class SubmissionValidationServiceIntegrationTest extends MockServerIntegrationTest {
 
+  private static final String OFFICE_CODE = "AQ2B3C";
+  private static final String AREA_OF_LAW = "CIVIL";
+
   @Autowired protected SubmissionValidationService submissionValidationService;
 
   UUID submissionId = UUID.fromString("0561d67b-30ed-412e-8231-f6296a53538d");
@@ -42,6 +47,13 @@ public class SubmissionValidationServiceIntegrationTest extends MockServerIntegr
       stubForUpdateSubmission(submissionId);
       stubReturnNoClaims(submissionId);
 
+      getStubForGetSubmissionByCriteria(
+          List.of(
+              Parameter.param("offices", OFFICE_CODE),
+              Parameter.param("area-of-law", AREA_OF_LAW),
+              Parameter.param("submission-period", "APR-2025")),
+          "data-claims/get-submission/get-submissions-by-filter_no_content.json");
+
       // When
       SubmissionValidationContext submissionValidationContext =
           submissionValidationService.validateSubmission(submissionId);
@@ -56,6 +68,13 @@ public class SubmissionValidationServiceIntegrationTest extends MockServerIntegr
       // Given
       stubForGetSubmission(submissionId, "data-claims/get-submission/get-submission-MAY-25.json");
       stubForUpdateSubmission(submissionId);
+
+      getStubForGetSubmissionByCriteria(
+          List.of(
+              Parameter.param("offices", OFFICE_CODE),
+              Parameter.param("area-of-law", AREA_OF_LAW),
+              Parameter.param("submission-period", "MAY-2025")),
+          "data-claims/get-submission/get-submissions-by-filter_no_content.json");
 
       // When
       SubmissionValidationContext submissionValidationContext =
@@ -76,6 +95,13 @@ public class SubmissionValidationServiceIntegrationTest extends MockServerIntegr
       stubForGetSubmission(submissionId, "data-claims/get-submission/get-submission-SEP-25.json");
       stubForUpdateSubmission(submissionId);
 
+      getStubForGetSubmissionByCriteria(
+          List.of(
+              Parameter.param("offices", OFFICE_CODE),
+              Parameter.param("area-of-law", AREA_OF_LAW),
+              Parameter.param("submission-period", "SEP-2025")),
+          "data-claims/get-submission/get-submissions-by-filter_no_content.json");
+
       // When
       SubmissionValidationContext submissionValidationContext =
           submissionValidationService.validateSubmission(submissionId);
@@ -86,6 +112,37 @@ public class SubmissionValidationServiceIntegrationTest extends MockServerIntegr
           submissionValidationContext,
           SubmissionValidationError.SUBMISSION_PERIOD_FUTURE_MONTH,
           "May 2025");
+    }
+
+    @DisplayName(
+        "Should have one error with submission period with combination of Office × Area of Law × Submission Period")
+    @Test
+    void shouldHaveOneErrorWithSubmissionPeriodWithCombinationOfOfficeAreaOfLawSubmissionPeriod()
+        throws Exception {
+      // Given
+      stubForGetSubmission(submissionId, "data-claims/get-submission/get-submission-APR-25.json");
+      stubForUpdateSubmission(submissionId);
+      stubReturnNoClaims(submissionId);
+
+      getStubForGetSubmissionByCriteria(
+          List.of(
+              Parameter.param("offices", OFFICE_CODE),
+              Parameter.param("area-of-law", AREA_OF_LAW),
+              Parameter.param("submission-period", "APR-2025")),
+          "data-claims/get-submission/get-submissions-by-filter.json");
+
+      // When
+      SubmissionValidationContext submissionValidationContext =
+          submissionValidationService.validateSubmission(submissionId);
+
+      // Then
+      assertThat(submissionValidationContext.getSubmissionValidationErrors().size()).isEqualTo(1);
+      assertContextClaimError(
+          submissionValidationContext,
+          SubmissionValidationError.SUBMISSION_ALREADY_EXISTS,
+          OFFICE_CODE,
+          AREA_OF_LAW,
+          "APR-2025");
     }
   }
 }
