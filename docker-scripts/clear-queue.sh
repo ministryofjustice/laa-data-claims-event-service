3528eb791a0524f121afe0c0b2dc51adf95414fd
vlorun-laa <victor.olorunleye1@justice.gov.uk>
2025-10-16 09:44:18 +0100

    removing local specific changes

--- docker-scripts/clear-queue.sh
+++ docker-scripts/clear-queue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Script to submit bulk claim submissions to localstack SQS queue (0s mock the account ID)
-docker exec claims-data-localstack-1 awslocal sqs purge-queue \
+docker exec event-service-localstack awslocal sqs purge-queue \
   --queue-url http://localhost:4566/000000000000/claims-api-queue
