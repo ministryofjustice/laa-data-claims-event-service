eb6c25c32aaaea9ba427acec5c1131a42eb32aa4
vlorun-laa <victor.olorunleye1@justice.gov.uk>
2025-10-16 11:23:01 +0100

    align branch with code changes and refactoring

--- data-claims-event-service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsevent/service/strategy/StrategyTypes.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.laa.dstew.payments.claimsevent.service.strategy;
-
-/** Strategy types for duplicate claim validation. */
-public class StrategyTypes {
-  public static final String CRIME = "CRIME_LOWER";
-  public static final String CIVIL = "CIVIL";
-  public static final String MEDIATION = "MEDIATION";
-}
