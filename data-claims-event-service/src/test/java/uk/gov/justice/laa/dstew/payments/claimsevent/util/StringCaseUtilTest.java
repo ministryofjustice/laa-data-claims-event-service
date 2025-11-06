7596836b7f2ac2520fa66cdd5da154dda76fb402
Jose Carlos Arinero Adam <jose.carlosarineroadam@justice.gov.uk>
2025-11-06 10:24:33 +0100

    DSTEW-710 Added unit test for StringCaseUtil

---
+++ data-claims-event-service/src/test/java/uk/gov/justice/laa/dstew/payments/claimsevent/util/StringCaseUtilTest.java
@@ -0,0 +1,26 @@
+package uk.gov.justice.laa.dstew.payments.claimsevent.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static uk.gov.justice.laa.dstew.payments.claimsevent.util.StringCaseUtil.toTitleCase;
+
+import org.junit.jupiter.api.Test;
+
+class StringCaseUtilTest {
+
+  @Test
+  void toTitleCaseTest() {
+    String invalidString = "";
+    String snakeCaseString = "snake_case";
+    String camelCaseString = "camelCase";
+    String snakeCaseWithExcludedLower = "excluded_of_snake";
+    String camelCaseWithExcludedCaps = "isNilSubmission";
+
+    assertThat(toTitleCase(invalidString)).isEqualTo(invalidString);
+    assertThat(toTitleCase(null)).isEqualTo(null);
+    assertThat(toTitleCase(snakeCaseString)).isEqualTo("Snake Case");
+    assertThat(toTitleCase(camelCaseString)).isEqualTo("Camel Case");
+    assertThat(toTitleCase(snakeCaseWithExcludedLower)).isEqualTo("Excluded of Snake");
+    assertThat(toTitleCase(camelCaseWithExcludedCaps)).isEqualTo("Is NIL Submission");
+  }
+}
