fdd104578e2bc376449e38909da4941a2dbdf3e7
Phil Payne <phil.payne@digital.justice.gov.uk>
2025-12-09 08:56:29 +0000

    Remove specific rate limiter revert to exponential backoff

--- data-claims-event-service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsevent/service/ProviderDetailsService.java
+++ data-claims-event-service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsevent/service/ProviderDetailsService.java
@@ -1,8 +1,5 @@
 package uk.gov.justice.laa.dstew.payments.claimsevent.service;
 
-import io.github.resilience4j.ratelimiter.RateLimiter;
-import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
-import io.github.resilience4j.reactor.ratelimiter.operator.RateLimiterOperator;
 import io.github.resilience4j.reactor.retry.RetryOperator;
 import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryRegistry;
@@ -26,7 +23,6 @@ public class ProviderDetailsService {
 
   private final ProviderDetailsRestClient providerDetailsRestClient;
   private final RetryRegistry retryRegistry;
-  private final RateLimiterRegistry rateLimiterRegistry;
 
   /**
    * Retrieves the provider firm office contract and schedule information for a given office, area
@@ -45,13 +41,9 @@ public class ProviderDetailsService {
    */
   public Mono<ProviderFirmOfficeContractAndScheduleDto> getProviderFirmSchedules(
       String officeCode, String areaOfLaw, LocalDate effectiveDate) {
-    RateLimiter perSecondLimiter = rateLimiterRegistry.rateLimiter("pdaRateLimiterPerSecond");
-    RateLimiter perMinuteLimiter = rateLimiterRegistry.rateLimiter("pdaRateLimiterPerMinute");
     Retry retry = retryRegistry.retry("pdaRetry");
     return providerDetailsRestClient
         .getProviderFirmSchedules(officeCode, areaOfLaw, effectiveDate)
-        .transformDeferred(RateLimiterOperator.of(perSecondLimiter))
-        .transformDeferred(RateLimiterOperator.of(perMinuteLimiter))
         .transformDeferred(RetryOperator.of(retry))
         .onErrorResume(
             e -> {
