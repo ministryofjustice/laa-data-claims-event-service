18d74bbd3f5e5d5b9d33423816e773d652131692
NagarjunaRachakonda <Nagarjuna.Rachakonda@justice.gov.uk>
2026-01-19 12:35:14 +0000

    DSTEW-997 add config

--- data-claims-event-service/src/pactTest/java/uk/gov/justice/laa/dstew/payments/claimsevent/AbstractPactTest.java
+++ data-claims-event-service/src/pactTest/java/uk/gov/justice/laa/dstew/payments/claimsevent/AbstractPactTest.java
@@ -1,14 +1,13 @@
 package uk.gov.justice.laa.dstew.payments.claimsevent;
 
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import software.amazon.awssdk.services.sqs.SqsClient;
-import uk.gov.justice.laa.dstew.payments.claimsevent.listener.SubmissionListener;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import uk.gov.justice.laa.dstew.payments.claimsevent.listener.SubmissionListener;
 
 public abstract class AbstractPactTest {
   public static final String CONSUMER = "laa-data-claims-event-service";
@@ -24,11 +23,9 @@ public abstract class AbstractPactTest {
   protected final UUID submissionId = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6");
   protected final UUID claimId = UUID.fromString("d4e3fa24-7d1f-4710-b7a7-0debe88421aa");
 
-  @MockitoBean
-  SubmissionListener submissionListener;
+  @MockitoBean SubmissionListener submissionListener;
 
-  @MockitoBean
-  SqsClient sqsClient;
+  @MockitoBean SqsClient sqsClient;
 
   public static String readJsonFromFile(final String fileName) throws Exception {
     Path path = Paths.get("src/pactTest/resources/responses/", fileName);
