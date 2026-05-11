0db5c378afb620de27370c5d686aa36a2f8f1607
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-05-11 16:35:10 +0100

    depend on localstack

--- docker-scripts/init-localstack.sh
+++ docker-scripts/init-localstack.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+echo "Initializing localstack SNS"
+TOPIC_ARN=$(awslocal sns create-topic --name claims-events --query 'TopicArn' --output text)
+
 echo "Initializing localstack SQS"
 
 QUEUE_URL=$(awslocal sqs create-queue --queue-name claims-api-queue --attributes VisibilityTimeout=1200 --query 'QueueUrl' --output text)
 QUEUE_ARN=$(awslocal sqs get-queue-attributes --queue-url $QUEUE_URL --attribute-name QueueArn --query 'Attributes.QueueArn' --output text)
-
-
-echo "Initializing localstack SNS"
-
-TOPIC_ARN=$(awslocal sns create-topic --name claims-events --query 'TopicArn' --output text)
-
 
 echo "Subscribing queue to topic"
 
