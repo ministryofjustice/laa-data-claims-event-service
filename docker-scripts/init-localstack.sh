b51fa76aa0ad8815dafac679461951316b4835bb
g-georgieva <gabriela.georgieva@justice.gov.uk>
2026-05-12 10:47:56 +0100

    add SNS fanout support for claims-event-service (#213)
    * add SNS fanout support for claims-event-service
    
    * depend on localstack
    
    * container name
    
    * container name

--- docker-scripts/init-localstack.sh
+++ docker-scripts/init-localstack.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo "Initializing localstack SNS"
+TOPIC_ARN=$(awslocal sns create-topic --name claims-events --query 'TopicArn' --output text)
+
 echo "Initializing localstack SQS"
 
+QUEUE_URL=$(awslocal sqs create-queue --queue-name claims-api-queue --attributes VisibilityTimeout=1200 --query 'QueueUrl' --output text)
+QUEUE_ARN=$(awslocal sqs get-queue-attributes --queue-url $QUEUE_URL --attribute-name QueueArn --query 'Attributes.QueueArn' --output text)
 
-awslocal sqs create-queue --queue-name claims-api-queue --attributes VisibilityTimeout=1200
+echo "Subscribing queue to topic"
+
+awslocal sns subscribe --topic-arn $TOPIC_ARN --protocol sqs --notification-endpoint $QUEUE_ARN --attributes RawMessageDelivery=true
