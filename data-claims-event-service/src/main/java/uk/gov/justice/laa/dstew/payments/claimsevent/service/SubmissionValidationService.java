b471503666c9e271626242d92175e4e704c6982f
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-08-05 16:22:01 +0100

    Remove 'saveAsDraft' functionality from bulk claim submissions
    This removes the 'saveAsDraft' option from the file upload process, related controllers, services, and validations. The corresponding UI elements, DTOs, and tests were updated to reflect this change. Removed unused references, properties, and client calls associated with this functionality.

--- data-claims-event-service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsevent/service/SubmissionValidationService.java
+++ data-claims-event-service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsevent/service/SubmissionValidationService.java
@@ -50,12 +50,6 @@ public class SubmissionValidationService {
     Assert.notNull(submission, "Submission not retrievable: " + submissionId.toString());
     SubmissionValidationContext context = initialiseValidationContext(submission);
 
-    var isDraftSubmission =
-        dataClaimsRestClient
-            .getBulkSubmission(submission.getBulkSubmissionId())
-            .getBody()
-            .getSaveAsDraft();
-
     // Currently validating:
     // - Submission Status (Has highest priority to update the submission status if required)
     // - Submission Schema
@@ -95,16 +89,12 @@ public class SubmissionValidationService {
           .errorDescription(
               "Validation completed for bulk submission %s with errors"
                   .formatted(bulkSubmissionId));
-    } else if (Boolean.TRUE.equals(isDraftSubmission)) {
+    } else {
       log.debug(
           "Validation completed for submission {} with no errors. Saving as draft", submissionId);
       submissionPatch.status(SubmissionStatus.READY_FOR_SUBMISSION);
-      bulkSubmissionPatch.status(BulkSubmissionStatus.READY_FOR_SUBMISSION);
-    } else {
-      log.debug("Validation completed for submission {} with no errors", submissionId);
-      submissionPatch.status(SubmissionStatus.VALIDATION_SUCCEEDED);
       eventServiceMetricService.incrementTotalValidSubmissions();
-      bulkSubmissionPatch.status(BulkSubmissionStatus.VALIDATION_SUCCEEDED);
+      bulkSubmissionPatch.status(BulkSubmissionStatus.READY_FOR_SUBMISSION);
     }
 
     // Record what submission errors were found
